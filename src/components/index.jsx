4b056bc73aa18f3e896046286f4c58e4d7b25871
yevhenvint <yevhenvint4dev@outlook.com>
2024-01-08 23:21:18 +0300

    Initial commit

---
+++ src/components/index.jsx
@@ -0,0 +1,33 @@
+import Button from './Button';
+import ThemeSettings from './ThemeSettings';
+import Sidebar from './Sidebar';
+import Navbar from './Navbar';
+import Footer from './Footer';
+import Cart from './Cart';
+import Chat from './Chat';
+import Notification from './Notification';
+import UserProfile from './UserProfile';
+import SparkLine from './Charts/SparkLine';
+import LineChart from './Charts/LineChart';
+import Stacked from './Charts/Stacked';
+import Pie from './Charts/Pie';
+import ChartsHeader from './ChartsHeader';
+import Header from './Header';
+
+export {
+  Button,
+  ThemeSettings,
+  Sidebar,
+  Navbar,
+  Footer,
+  Cart,
+  Chat,
+  Notification,
+  UserProfile,
+  SparkLine,
+  LineChart,
+  Stacked,
+  Pie,
+  ChartsHeader,
+  Header,
+};
